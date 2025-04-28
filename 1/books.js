4409b1163c7ad16fcf05b5e63e570724953717e9
cablex126 <136658513+cablex126@users.noreply.github.com>
2025-04-28 10:05:21 +0800

    Add files via upload

---
+++ 1/books.js
@@ -0,0 +1,30 @@
+[
+    {
+        "id": 1,
+        "title": "The Silent Voyage",
+        "author": "Emily Rivers",
+        "genre": "Science Fiction",
+        "available": true
+    },
+    {
+        "id": 2,
+        "title": "Whispers of the Past",
+        "author": "James Holloway",
+        "genre": "Historical Fiction",
+        "available": true
+    },
+    {
+        "id": 3,
+        "title": "The Last Ember",
+        "author": "Sophie Williams",
+        "genre": "Fantasy",
+        "available": true
+    },
+    {
+        "id": 4,
+        "title": "Shadows in the Mist",
+        "author": "Liam Andrews",
+        "genre": "Mystery/Thriller",
+        "available": true
+    }
+]
