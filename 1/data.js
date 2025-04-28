e41170688d5faf7607ac37a83163396e329a54cc
cablex126 <136658513+cablex126@users.noreply.github.com>
2025-04-28 09:42:30 +0800

    Add files via upload

---
+++ 1/data.js
@@ -0,0 +1,20 @@
+[
+    {
+    "id": 1,
+    "name": "John Doe",
+    "email": "sample@grc.com",
+    "phone": "+1-202-555-0173",
+    "address": {
+        "street": "21 panghulo Malabon",
+        "city": "Anytown",
+        "state": "CA",
+        "zip": "1470"
+    },
+    "company": {
+        "name": "Tech Solutions",
+        "position": "Software Engineer",
+        "department": "Development"
+    }
+
+}
+]
